162ca2e4bd0046b10115eddd9ba58394bfee4d06
Adam La Morre <alamorre@gmail.com>
2021-04-24 13:20:59 -0400

    ready to auth and create user

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,35 @@
-import React from "react"
-import { Card, Button } from "react-bootstrap"
+import React, { useRef, useEffect } from "react"
+import { Button } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 import { auth } from "../firebase"
 
 export default function Dashboard() {
+  const didMountRef = useRef(false)
   const { currentUser } = useAuth()
   const history = useHistory()
 
   async function handleLogout() {
-    try {
-      await auth.signOut()
-      history.push("/auth")
-    } catch {
-      console.log('error')
-    }
+    await auth.signOut()
+    history.push("/auth")
   }
 
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
 
+      const userName = currentUser.email
+      const userEmail = userName
+      const userAvatar = currentUser.photoURL
+      const userSecret = currentUser.uid
+      
+      console.log(userName, userEmail, userAvatar, userSecret)
+    }
+  }, [currentUser])
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
       <div className="w-100 text-center mt-2">
         <Button variant="link" onClick={handleLogout}>
           Log Out
