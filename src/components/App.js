0c2f568c3ae531a5f162db6bbfa34e44b21c2914
Adam La Morre <alamorre@gmail.com>
2021-04-24 20:23:25 -0400

    no CSS

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,22 @@ import React from "react"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import './App.css'
-
 import Dashboard from "./Dashboard"
 import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Switch>
-          <PrivateRoute exact path="/" component={Dashboard} />
-          <Route path="/auth" component={Login} />
-        </Switch>
-      </AuthProvider>
-    </Router>
+    <div style={{ fontFamily: 'Avenir' }}>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <Route path="/auth" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
   )
 }
 
