cee99fc3e1afbe1f09193f0d71c592a7c983eae6
Adam La Morre <alamorre@gmail.com>
2021-04-28 20:17:10 -0400

    refactor login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,40 +8,23 @@ import { useAuth } from "../contexts/AuthContext"
 
 export default function Login() {
   const { login } = useAuth()
-  
-  function googleLogin() { login(new firebase.auth.GoogleAuthProvider()) }
-  function facebookLogin() { login(new firebase.auth.FacebookAuthProvider()) }
 
   return (
     <div>
       <h2>Welcome</h2>
 
-      <div 
-        onClick={() => googleLogin()}
-        style={{ 
-          cursor: 'pointer', 
-          backgroundColor: '#1890ff', 
-          color: 'white', 
-          padding: '12px', 
-          borderRadius: '8px', 
-          display: 'inline-block' 
-        }}
+      <div
+        className='login-button'
+        onClick={() => login(new firebase.auth.GoogleAuthProvider())}
       >
         <GoogleOutlined /> Sign In with Google
       </div>
 
       <br /> <br />
 
-      <div 
-        onClick={() => facebookLogin()}
-        style={{ 
-          cursor: 'pointer', 
-          backgroundColor: '#1890ff', 
-          color: 'white', 
-          padding: '12px', 
-          borderRadius: '8px', 
-          display: 'inline-block' 
-        }}
+      <div
+        className='login-button'
+        onClick={() => login(new firebase.auth.FacebookAuthProvider())}
       >
         <FacebookOutlined /> Sign In with Facebook
       </div>
